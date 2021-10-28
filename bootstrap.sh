3a107718c1ac35b773a8cae41f46f7943e1357f0
NameNoQQ <89968152+NameNoQQ@users.noreply.github.com>
2021-10-28 13:49:18 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,4 @@ sudo pacman -S docker
 sudo systemctl enable docker --now
 
 # installing Kubernetes
-sudo pacman -S kubeadm kubelet kubectl kube-apiserver kube-controller-manager kubectl-cert_manager kubect-ingress_nginx kubectx kube-linter kubens kubeone kube-proxy kube-scheduler
-swapoff -a
-sudo pacman -Syu
-sudo pacman -S docker
-sudo systemctl enable docker --now
+sudo pacman -S kubeadm kubelet kubectl kube-apiserver kube-controller-manager kubectl-cert-manager kubectl-ingress-nginx kubectx kube-linter kubens kubeone kube-proxy kube-scheduler
